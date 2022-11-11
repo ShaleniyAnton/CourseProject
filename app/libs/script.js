3b3f587f15b476e75ac045dd1d1531e911145e78
ShaleniyAnton <toha4706@gmail.com>
2022-11-11 13:57:43 +0200

    Course Project

---
+++ app/libs/script.js
@@ -0,0 +1,11 @@
+$(function() {
+
+
+  $('#coin-slider').coinslider({width:960,height:360,opacity:1});
+
+  $('.menu_nav ul li a').css({"border-top-left-radius":"6px", "border-top-right-radius":"6px", "-moz-border-radius-topleft":"6px", "-moz-border-radius-topright":"6px", "-webkit-border-top-left-radius":"6px", "-webkit-border-top-right-radius":"6px"});
+
+});	
+
+
+
